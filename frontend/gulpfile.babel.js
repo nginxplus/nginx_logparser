1ee6821b9d41617975919bb55d4d1a4df9ce10ef
cornBuddy <heorhi.sakovich@gmail.com>
2016-06-26 18:19:09 +0300

    lint and test command setting up

--- frontend/gulpfile.babel.js
+++ frontend/gulpfile.babel.js
@@ -1,5 +1,23 @@
-import { gulp } from 'gulp';
+import gulp from 'gulp';
+import eslint from 'gulp-eslint';
+import mocha from 'gulp-spawn-mocha';
 
-export default function(cb) {
-  cb();
+const isDevelopment = !process.env.ENV
+    || process.env.ENV === 'development';
+
+export function lint() {
+  return gulp.src(['src/**/*.js', 'test/**/*.js'], {
+        since: gulp.lastRun('lint'),
+        read: false,
+      })
+      .pipe(eslint())
+      .pipe(eslint.format());
+}
+
+export function test() {
+  return gulp.src('test/**/*.js', {
+        since: gulp.lastRun('test'),
+        read: false,
+      })
+      .pipe(mocha());
 }
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,4 +1,10 @@
+/**
+ * Class representing main page
+ */
 export class Welcome {
+  /**
+   * Create a main page
+   */
   constructor() {
     this.heading = 'it works!';
   }
