10344254b835013cd833e647be723863aa285730
cornBuddy <heorhi.sakovich@gmail.com>
2016-06-26 19:32:43 +0300

    dev environment ready

--- frontend/gulpfile.babel.js
+++ frontend/gulpfile.babel.js
@@ -1,23 +1,39 @@
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';
 import mocha from 'gulp-spawn-mocha';
+import clear from 'clear';
 
 const isDevelopment = !process.env.ENV
     || process.env.ENV === 'development';
 
+const pathTo = {
+  src: 'src/**/*.js',
+  tests: 'test/**/*.js',
+};
+
+export function clearConsole(cb) {
+  clear();
+  cb();
+}
+
 export function lint() {
-  return gulp.src(['src/**/*.js', 'test/**/*.js'], {
-        since: gulp.lastRun('lint'),
-        read: false,
-      })
-      .pipe(eslint())
-      .pipe(eslint.format());
+  return gulp.src([pathTo.src, pathTo.tests], {
+      since: gulp.lastRun('lint'),
+      read: false,
+    })
+    .pipe(eslint())
+    .pipe(eslint.format());
 }
 
 export function test() {
-  return gulp.src('test/**/*.js', {
-        since: gulp.lastRun('test'),
-        read: false,
-      })
-      .pipe(mocha());
+  return gulp.src(pathTo.tests, {
+      since: gulp.lastRun('test'),
+      read: false,
+    })
+    .pipe(mocha());
+}
+
+export default function dev() {
+  gulp.watch([pathTo.src, pathTo.tests],
+    gulp.series('clearConsole', 'lint', 'test'));
 }
--- frontend/test/test.js
+++ frontend/test/test.js
@@ -1,7 +1,1 @@
-import { expect } from 'chai';
 
-describe('test', () => {
-  it('test', () => {
-    expect(true).to.be.true;
-  })
-})
