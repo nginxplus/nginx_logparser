b9026bce2dd16c42146739013bf57230de318d4b
Nikita Pokotulo <pokotulonikita@gmail.com>
2016-07-15 23:01:22 +0300

    update parcer (v1)

--- usr/share/nginx_logparser/parser.py
+++ usr/share/nginx_logparser/parser.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python
+
 import re
 import sqlite3
 
 
 def push_sqlite(data):
 
-	#conn = sqlite3.connect('/usr/share/nginx_logparser/nginx_logs.db')
-	#conn.isolation_level = None
-	#cur = conn.cursor()
-	#req = 'INSERT INTO access(ip, date, method, uri, code, domain, platform) VALUES (\'' + data["ip"] + '\', \'' + data["date"] + '\', \'' + data["method"] + '\', \'' + data["uri"] + '\', \'' + data["code"] + '\', \'' + data["domain"] + '\', \'' + data["platform"] + '\');'
-
-	print data
-	#conn.close()
+	conn = sqlite3.connect('/usr/share/nginx_logparser/nginx_logs.db')
+	conn.isolation_level = None
+	cur = conn.cursor()
+	req = 'INSERT INTO access(ip, date, method, uri, code, domain, platform) VALUES (\'' + data["ip"] + '\', \'' + data["date"] + '\', \'' + data["method"] + '\', \'' + data["uri"] + '\', \'' + data["code"] + '\', \'' + data["domain"] + '\', \'' + data["platform"] + '\');'
+	#print data
+	conn.close()
 
 
 def main():
 	log_path = "/var/log/nginx/"
 	log_file = open(log_path + "access.log.1", "r")
 	line = log_file.readline()
-	valid_pages = ["/", "/portfolio" ,"/contacts", "/about"]
-
 
 	while line != "":
 
@@ -30,7 +28,7 @@ def main():
 			date = re.search(r"[0-9]*/[A-Za-z]{3}/[^ ]+", line).group(0)
 			client_req = re.search(r'\"[A-Z]{3,}[^\"]*', line).group(0)[1:]
 			uri = client_req[(client_req.find(" "))+1:(client_req.rfind(" "))]
-			method = client_req[:(client_req.find(" "))] 
+			method = client_req[:(client_req.find(" "))]
 			code = re.search(r"[ ][0-9]{3}[ ]", line).group(0)[1:-1]
 			client_platform = re.search(r'\"[^\"]*\"$', line).group(0)[1:-1]
 			domain = re.search(r'\"http\:\/\/[^\/]*\/', line)
@@ -40,15 +38,14 @@ def main():
 			else:
 				domain = domain.group(0)[1:]
 
-			if valid_pages.count(page) is not 0:
 
-				connection_table["ip"] = ip
-				connection_table["date"] = date
-				connection_table["method"] = method
-				connection_table["uri"] = uri
-				connection_table["code"] = code
-				connection_table["domain"] = domain
-				connection_table["platform"] = client_platform
+			connection_table["ip"] = ip
+			connection_table["date"] = date
+			connection_table["method"] = method
+			connection_table["uri"] = uri
+			connection_table["code"] = code
+			connection_table["domain"] = domain
+			connection_table["platform"] = client_platform
 
 		except Exception as e:
 			print "Something is wrong here: ", e
