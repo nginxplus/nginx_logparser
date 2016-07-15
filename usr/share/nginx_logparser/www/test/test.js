977388999e231279376caa498752c00ca4ce4e57
cornBuddy <heorhi.sakovich@gmail.com>
2016-07-15 20:36:44 +0300

    fixture added

--- usr/share/nginx_logparser/www/test/test.js
+++ usr/share/nginx_logparser/www/test/test.js
@@ -1,1 +1,9 @@
+const expect = require('chai').expect;
+const readFile = require('fs').readFile;
 
+let testObject = null;
+readFile(`${__dirname}/fixture.json`, (err, data) => {
+  if (err)
+    throw err;
+  testObject = data;
+});
