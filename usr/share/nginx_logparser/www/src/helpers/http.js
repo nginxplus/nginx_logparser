4039fdc891fb88f6389158cc031ee6070fb6540b
cornBuddy <heorhi.sakovich@gmail.com>
2016-07-17 20:04:04 +0300

    refactoring

--- usr/share/nginx_logparser/www/src/helpers/http.js
+++ usr/share/nginx_logparser/www/src/helpers/http.js
@@ -5,23 +5,40 @@ import fetch from 'isomorphic-fetch';
  */
 export default class Http {
   /**
+   * Method checks http status code for successivity
+   * @param {Number} code - Numeric representation of http status code
+   * @return {Boolean} True if code is successful, else false
+   */
+  static _isSuccessCode(code) {
+    const SUCCESS_CODE = 200;
+    const REDIRECT_CODE = 300;
+    return code >= SUCCESS_CODE && code < REDIRECT_CODE;
+  }
+  /**
+   * Method checks results for a status code and return it if it's okay
+   * @param {http-response-object} response - Http response object
+   * @returns {http-response-object} Same as input
+   * @throws {Error} If bad code
+   */
+  static _checkStatus(response) {
+    // TODO: what should we do on redirections?
+    if (this._isSuccessCode(response.status))
+      return response;
+    else {
+      const error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }
+  }
+  /**
    * Method represents http get request
    * @param {string} url - Absolute URL
    * @returns {promise} http response
    */
   static get(url) {
-    const checkStatus = (response) => {
-      // TODO: what should we do on redirections?
-      if (response.status >= 200 && response.status <= 300)
-        return response;
-      else {
-        let error = new Error(response.statusText);
-        error.response = response;
-        throw error;
-      }
-    }
     // TODO: what should we do on errors?
     return fetch(url)
-        .then(checkStatus);
+        .then(this.checkStatus)
+        .then(this.parseJson);
   }
 }
