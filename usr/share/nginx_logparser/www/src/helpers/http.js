1e7337deaf8ec81673129ec30caa53ab6490eaa4
cornBuddy <heorhi.sakovich@gmail.com>
2016-07-17 17:59:05 +0300

    http status checking added

--- usr/share/nginx_logparser/www/src/helpers/http.js
+++ usr/share/nginx_logparser/www/src/helpers/http.js
@@ -1,4 +1,5 @@
 import fetch from 'isomorphic-fetch';
+
 /**
  * Class represents http requests layer to API
  */
@@ -9,6 +10,18 @@ export default class Http {
    * @returns {promise} http response
    */
   static get(url) {
-    return fetch(url);
+    const checkStatus = (response) => {
+      // TODO: what should we do on redirections?
+      if (response.status >= 200 && response.status <= 300)
+        return response;
+      else {
+        let error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      }
+    }
+    // TODO: what should we do on errors?
+    return fetch(url)
+        .then(checkStatus);
   }
 }
