2d9768e41d9bcbb6e97a47cb21fe197d9ce6d612
cornBuddy <heorhi.sakovich@gmail.com>
2016-07-17 19:31:33 +0300

    few specs added

--- usr/share/nginx_logparser/www/test/http.test.js
+++ usr/share/nginx_logparser/www/test/http.test.js
@@ -1,5 +1,9 @@
-import { expect } from 'chai';
+import chai from 'chai';
+const expect = chai.expect;
 import { readFile } from 'fs';
+import chaiAsPromised from 'chai-as-promised';
+chai.use(chaiAsPromised);
+
 import Http from './../src/helpers/http.js';
 
 let testObject = null;
@@ -11,15 +15,23 @@ readFile(`${__dirname}/fixture.json`, (err, data) => {
 
 describe('Http', function() {
   it('should be a function', function() {
-    expect(Http).to.be.a('function');
+    return expect(Http).to.be.a('function');
   });
 
   describe('#get', function() {
     it('should be function', function() {
-      expect(Http.get).to.be.a('function');
+      return expect(Http.get).to.be.a('function');
     });
     it('should return promise', function() {
-      expect(Http.get()).to.be.a('promise');
+      return expect(Http.get()).to.be.a('promise');
+    });
+    it('should be rejected on null', function() {
+      const promise = Http.get(null);
+      return expect(promise).to.be.rejectedWith(Error);
+    });
+    it('should be rejected on empty argumets', function() {
+      const promise = Http.get();
+      return expect(promise).to.be.rejectedWith(Error);
     });
   });
 });
