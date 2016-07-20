63b2e9588066d902c36dedc50809ca856d3a4f08
cornBuddy <heorhi.sakovich@gmail.com>
2016-07-21 00:21:36 +0300

    specs implemented

--- usr/share/nginx_logparser/www/src/helpers/model.js
+++ usr/share/nginx_logparser/www/src/helpers/model.js
@@ -1,6 +1,16 @@
+import Http from './http';
+
 /**
  * Class represents data access layer to server
  */
 export default class Model {
-
+  /**
+   * Function makes request to url and parse response to JSON
+   * @param {String} url - Url to a remote resource
+   * @returns {Promise} JSON object
+   */
+  static _parse(url) {
+    return Http.get(url)
+      .then((body) => JSON.parse(body));
+  }
 }
