328e6693feccd3719b1569be1aa12d80a81ef0b4
cornBuddy <heorhi.sakovich@gmail.com>
2016-07-21 00:55:32 +0300

    constructor and get property implemented

--- usr/share/nginx_logparser/www/src/helpers/model.js
+++ usr/share/nginx_logparser/www/src/helpers/model.js
@@ -5,11 +5,29 @@ import Http from './http';
  */
 export default class Model {
   /**
+   * Creates new model instanse with given url
+   * @param {String} url - Url to remote resource
+   * @constructor
+   */
+  constructor(url) {
+    if (typeof url !== 'string' || !url)
+      throw new TypeError(`Url expected, ${url} given`);
+    this._url = url;
+  }
+  /**
+   * Returns model url
+   * @returns {String} Url to remote resource
+   */
+  get url() {
+    return this._url;
+  }
+  /**
    * Function makes request to url and parse response to JSON
    * @param {String} url - Url to a remote resource
    * @returns {Promise} JSON object
    */
   static _parse(url) {
+    // TODO: on server, when there is bad url, return 400
     return Http.get(url)
       .then((body) => JSON.parse(body));
   }
