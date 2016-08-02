6885d589c5183d01ab23bcc8b05a8e32d18dab1c
cornBuddy <heorhi.sakovich@gmail.com>
2016-08-02 16:54:57 +0300

    basic aurelia entry point configuration added

---
+++ usr/share/nginx_logparser/www/src/entry.js
@@ -0,0 +1,10 @@
+/**
+ * The entry point of aurelia application
+ * @param {Aurelia} aurelia -- Main aurelia object
+ * @returns {undefined}
+ */
+export function configure(aurelia) {
+  aurelia.use.basicConfiguration();
+  aurelia.start()
+    .then(() => aurelia.setRoot());
+}
